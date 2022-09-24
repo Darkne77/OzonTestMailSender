89cd93e85a530ce2c474370c37a851d79a3e1f85
Leo <rumiantsev.l@gmail.com>
2022-09-24 16:53:31 +0300

    Add Test project

---
+++ OzonTestMailSender.Core.Tests/Models/EmailMessageTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using OzonTestMailSender.Core.Exceptions;
+using OzonTestMailSender.Core.Models;
+
+namespace OzonTestMailSender.Core.Tests.Models;
+
+public class EmailMessageTests
+{
+    [TestCase(null, "SomeSubject", "SomeText")]
+    [TestCase("", "SomeSubject", "SomeText")]
+    [TestCase(" ", "SomeSubject", "SomeText")]
+    public void ConstructWithExceptionResult(string recipient, string subject, string text)
+    {
+        var carbonCopyRecipients = Array.Empty<string>();
+        
+        Action act = () => new EmailMessage(recipient, subject, text, carbonCopyRecipients);
+
+        act.Should().Throw<ValidationException>();
+    }
+}
